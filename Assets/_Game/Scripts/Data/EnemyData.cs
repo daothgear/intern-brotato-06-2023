13d44c6b557a4cf1be0ad42a110c5f1b09d7ae30
Quang Huy <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-10 20:14:32 +0700

    Update Data + Ui

---
+++ Assets/_Game/Scripts/Data/EnemyData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyData
+{
+    [System.Serializable]
+    public class EnemyInfo
+    {
+        public int enemyID;
+        public float moveSpeed;
+        public int maxHP;
+        public int meleeAttackDamage;
+        public float meleeAttackRange;
+        public float meleeAttackSpeed;
+        public int gunAttackDamage;
+        public float gunAttackRange;
+        public float gunAttackSpeed;
+        public float laoAttackSpeed;
+    }
+    public List<EnemyInfo> enemyInfo = new List<EnemyInfo>();
+}
---
+++ Assets/_Game/Scripts/Data/WeaponLevelData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponLevelData
+{
+    [System.Serializable]
+    public class WeaponInfo
+    {
+        public int cost;
+        public int damage;
+        public float attackRange;
+        public float attackSpeed;
+        public int pierce;
+        public float pierceDamageReduce;
+    }
+
+    public List<WeaponInfo> weaponInfo = new List<WeaponInfo>();
+}
