990a486f028981ae315ac29e40292d35e246f71b
Qhuy9701 <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-12 14:43:47 +0700

    Working on level design
    Working on level design

--- Assets/_Game/Scripts/Ui/CharacterSelect/CharacterInfoCanvas.cs
+++ Assets/_Game/Scripts/Ui/CharacterSelect/CharacterInfoCanvas.cs
@@ -9,4 +9,11 @@ public class CharacterInfoCanvas : MonoBehaviour
         int previousSceneIndex = SceneManager.GetSceneByName(currentSceneName).buildIndex - 1;
         SceneManager.LoadScene(previousSceneIndex);
     }
+
+    public void NextToPreviousScene()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        int nextSceneIndex = SceneManager.GetSceneByName(currentSceneName).buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
 }
