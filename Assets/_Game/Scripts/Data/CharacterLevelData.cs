127f7231f48ed8d8fa0ae2da5d785da1731d5845
Quang Huy <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-12 22:20:49 +0700

    Upload data json + Change Ui + Update PlayerMove
    Upload JSON data from the player file + Updated flip image function when player move + Update Start Game video UI.

--- Assets/_Game/Scripts/Data/CharacterLevelData.cs
+++ Assets/_Game/Scripts/Data/CharacterLevelData.cs
@@ -9,6 +9,7 @@ public class CharacterLevelData
     public class CharacterInfo
     {
         public int characterID;
+        public int exp;
         public int maxHP;
         public float moveSpeed;
     }
--- Assets/_Game/Scripts/Player/PlayerMove.cs
+++ Assets/_Game/Scripts/Player/PlayerMove.cs
@@ -6,94 +6,113 @@ using System.IO;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] private Joystick joystick;
+  [SerializeField] private Animator animator;
+  [SerializeField] private Joystick joystick;
+  [SerializeField] private GameObject[] weapons;
+  [SerializeField] private float speed;
+  [SerializeField] private CharacterLevelData characterLevelData;
 
-    [SerializeField] private GameObject weapon_1;
-    [SerializeField] private GameObject weapon_2;
-    [SerializeField] private GameObject weapon_3;
-    [SerializeField] private GameObject weapon_4;
-    [SerializeField] private GameObject weapon_5;
+  private CharacterLevelData.CharacterInfo currentCharacterInfo;
+  private bool isFacingRight = true;
 
-    [SerializeField] private float speed;
-    
-    [SerializeField] private CharacterLevelData characterLevelData; 
-    [SerializeField] private CharacterLevelData.CharacterInfo currentCharacterInfo;
+  private void Start()
+  {
+    animator = GetComponent<Animator>();
+    LoadCharacterInfo(1);
+  }
 
-    private void Start()
+  private void Update()
+  {
+    Move();
+
+    if ( ShouldFlip() )
     {
-        animator = GetComponent<Animator>();
-        LoadCharacterInfo(1);
+      Flip();
+    }
+  }
+
+  private void Move()
+  {
+    Vector3 movement = new Vector3(joystick.Horizontal , joystick.Vertical , 0) * Time.deltaTime * speed;
+    transform.position += movement;
+
+    if ( movement.magnitude > 0 )
+    {
+      animator.SetTrigger("Walk");
+    }
+    else
+    {
+      animator.SetTrigger("Idle");
+    }
+  }
+
+  private bool ShouldFlip()
+  {
+    if ( joystick.Horizontal < 0 && isFacingRight )
+    {
+      return true;
+    }
+    else if ( joystick.Horizontal > 0 && !isFacingRight )
+    {
+      return true;
     }
 
-    private void Update()
+    return false;
+  }
+
+  private void Flip()
+  {
+    isFacingRight = !isFacingRight;
+    Vector3 scale = transform.localScale;
+    scale.x *= -1;
+    transform.localScale = scale;
+  }
+
+  private void LoadCharacterInfo( int currentLevel )
+  {
+    string characterLevelPath = Path.Combine(Application.streamingAssetsPath , "CharacterLevelData.json");
+    if ( File.Exists(characterLevelPath) )
     {
-        Move();
-    }
+      string characterLevelJson = File.ReadAllText(characterLevelPath);
+      characterLevelData = JsonConvert.DeserializeObject<CharacterLevelData>(characterLevelJson);
 
-    private void Move()
+      foreach ( var characterInfo in characterLevelData.characterInfo )
+      {
+        if ( characterInfo.characterID == currentLevel )
+        {
+          currentCharacterInfo = characterInfo;
+          Debug.Log("Character level data loaded successfully.");
+          speed = currentCharacterInfo.moveSpeed;
+          break;
+        }
+      }
+
+      if ( currentCharacterInfo == null )
+      {
+        Debug.LogError("Character info not found for level: " + currentLevel);
+      }
+    }
+    else
     {
-        transform.position += new Vector3(joystick.Horizontal, joystick.Vertical, 0) * Time.deltaTime * speed;
-        if (joystick.Horizontal!= 0 || joystick.Vertical!= 0)
-        {
-            animator.SetTrigger("Walk");
-        }
-        else
-        {
-            animator.SetTrigger("Idle");
-        }
+      Debug.LogError("File not found: " + characterLevelPath);
     }
+  }
 
-    private void LoadCharacterInfo(int currentLevel)
+  private void OnTriggerEnter2D( Collider2D collision )
+  {
+    if ( collision.gameObject.tag == "UpLevel" )
     {
-        // Load JSON data from file
-        string characterLevelPath = Path.Combine(Application.streamingAssetsPath, "CharacterLevelData.json");
-        if (File.Exists(characterLevelPath))
-        {
-            string characterLevelJson = File.ReadAllText(characterLevelPath);
-            characterLevelData = JsonConvert.DeserializeObject<CharacterLevelData>(characterLevelJson);
-
-            // Find the CharacterInfo for the current level
-            foreach (var characterInfo in characterLevelData.characterInfo)
-            {
-                if (characterInfo.characterID == currentLevel)
-                {
-                    currentCharacterInfo = characterInfo;
-                    Debug.Log("Character level data loaded successfully.");
-
-                    // Set the speed based on moveSpeed from JSON
-                    speed = currentCharacterInfo.moveSpeed;
-
-                    break;
-                }
-            }
-
-            if (currentCharacterInfo == null)
-            {
-                Debug.LogError("Character info not found for level: " + currentLevel);
-            }
-        }
-        else
-        {
-            Debug.LogError("File not found: " + characterLevelPath);
-        }
+      LoadCharacterInfo(currentCharacterInfo.characterID + 1);
+      animator.SetTrigger("UpLevel");
+      Debug.Log("Trigger");
     }
+  }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+  private void OnCollisionEnter2D( Collision2D collision )
+  {
+    if ( collision.gameObject.tag == "Wall" )
     {
-        if (collision.gameObject.tag == "UpLevel")
-        {
-            LoadCharacterInfo(currentCharacterInfo.characterID + 1);
-            animator.SetTrigger("UpLevel");
-            Debug.Log("Trigger");
-        }
+      Debug.Log("Collision");
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Wall")
-        {
-            Debug.Log("Collision");
-        }
-    }
+  }
 }
