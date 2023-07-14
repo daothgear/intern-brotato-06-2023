c72d55220634b26459ea6f8310171519afc394e2
Qhuy9701 <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-14 14:39:45 +0700

    Upload Data Enemy
    Upload Data Enemy

--- Assets/_Game/Scripts/Enemy/Enemy.cs
+++ Assets/_Game/Scripts/Enemy/Enemy.cs
@@ -39,7 +39,7 @@ public class Enemy : MonoBehaviour
 
   private void Idle()
   {
-
+    enemyState = EnemyState.Walk;
   }
 
   private void Walk()
---
+++ Assets/_Game/Scripts/Enemy/EnemyLoader.cs
@@ -0,0 +1,64 @@
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class EnemyLoader : MonoBehaviour
+{
+  private EnemyLoader enemyLoader;
+  private EnemyData enemyData;
+
+  //Data
+  [SerializeField] private float moveSpeed;
+  [SerializeField] private int maxHP;
+  [SerializeField] private int meleeAttackDamage;
+  [SerializeField] private float meleeAttackRange;
+  [SerializeField] private float meleeAttackSpeed;
+  [SerializeField] private int gunAttackDamage;
+  [SerializeField] private float gunAttackRange;
+  [SerializeField] private float gunAttackSpeed;
+  [SerializeField] private float laoAttackSpeed;
+  [SerializeField] private int currentLevel = 1;
+
+  private void Awake()
+  {
+    LoadEnemyInfo(currentLevel);
+  }
+
+  public void LoadEnemyInfo(int enemyID)
+  {
+    string enemyDataPath = Path.Combine(Application.streamingAssetsPath, "EnemyData.json");
+    if (File.Exists(enemyDataPath))
+    {
+      string enemyDataJson = File.ReadAllText(enemyDataPath);
+      enemyData = JsonConvert.DeserializeObject<EnemyData>(enemyDataJson);
+
+      foreach (var enemyInfo in enemyData.enemyInfo)
+      {
+        if (enemyInfo.enemyID == enemyID)
+        {
+          EnemyData.EnemyInfo currentEnemyInfo = enemyInfo;
+          Debug.Log("Enemy data loaded successfully.");
+          moveSpeed = currentEnemyInfo.moveSpeed;
+          maxHP = currentEnemyInfo.maxHP;
+          meleeAttackDamage = currentEnemyInfo.meleeAttackDamage;
+          meleeAttackRange = currentEnemyInfo.meleeAttackRange;
+          meleeAttackSpeed = currentEnemyInfo.meleeAttackSpeed;
+          gunAttackDamage = currentEnemyInfo.gunAttackDamage;
+          gunAttackRange = currentEnemyInfo.gunAttackRange;
+          gunAttackSpeed = currentEnemyInfo.gunAttackSpeed;
+          laoAttackSpeed = currentEnemyInfo.laoAttackSpeed;
+          break;
+        }
+      }
+
+      if (enemyData == null)
+      {
+        Debug.LogError("Enemy info not found for enemy ID: " + enemyID);
+      }
+    }
+    else
+    {
+      Debug.LogError("File not found: " + enemyDataPath);
+    }
+  }
+}
