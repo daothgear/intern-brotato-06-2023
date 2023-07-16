6e243052c7d2c2a0d476c032b528b79489209800
Quang Huy <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-17 01:12:29 +0700

    Update WaveManager
    Update WaveManager

--- Assets/_Game/Scripts/Data/WaveData.cs
+++ Assets/_Game/Scripts/Data/WaveData.cs
@@ -1,20 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 [System.Serializable]
 public class WaveData
 {
-	public WaveInfo[] waveInfo;
+  public WaveInfo[] waveInfo;
 
-	[System.Serializable]
-	public class WaveInfo
-	{
-		public int waveID;
-		public float totalTime;
-		public float weaponBuyableAfterFinish;
-		public int[] subWaveTimeArray;
-		public int[] subWave1stSpawnEnemyData;
-		public int subWaveSpawnIncreasePercentage;
-	}
+  [System.Serializable]
+  public class WaveInfo
+  {
+    public int waveID;
+    public float totalTime;
+    public float weaponBuyableAfterFinish;
+    public SubWaveInfo[] subWaves;
+  }
+
+  [System.Serializable]
+  public class SubWaveInfo
+  {
+    public float duration;
+    public int numEnemies;
+  }
 }
