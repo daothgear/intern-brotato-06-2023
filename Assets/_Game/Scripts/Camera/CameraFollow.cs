df6e395f80043b7a273b88c5fbfaeb2c48071ac7
Quang Huy <136431938+GearIncQhuy@users.noreply.github.com>
2023-07-30 23:59:59 +0700

    Update camera
    Update camera

--- Assets/_Game/Scripts/Camera/CameraFollow.cs
+++ Assets/_Game/Scripts/Camera/CameraFollow.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-  [SerializeField] private Transform target;
-  [SerializeField] private float smoothSpeed = 16f;
-  [SerializeField] private Vector3 offset;
+  [SerializeField] private Transform player;
+  [SerializeField] private Vector2 offset;
+  [SerializeField] private float smoothSpeed = 0.125f;
+  [SerializeField] private float leftLimit = -10f;
+  [SerializeField] private float rightLimit = 22f;
+  [SerializeField] private float topLimit = 21f;
+  [SerializeField] private float bottomLimit = -23f;
 
-  private PlayerFollow playerFollow {
-    get => PlayerFollow.Instance;
-  }
-  private void Update() {
-    Vector3 desiredPosition = playerFollow.player.transform.position + offset;
-    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-    transform.position = smoothedPosition;
+  private void LateUpdate() {
+    if (player == null) {
+      player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    if (player != null) {
+      Vector3 desiredPosition = new Vector3(player.position.x + offset.x , player.position.y + offset.y , transform.position.z);
+      Vector3 smoothedPosition = Vector3.Lerp(transform.position , desiredPosition , smoothSpeed);
+      smoothedPosition.x = Mathf.Clamp(smoothedPosition.x , leftLimit , rightLimit);
+      smoothedPosition.y = Mathf.Clamp(smoothedPosition.y , bottomLimit , topLimit);
+      transform.position = smoothedPosition;
+    }
   }
 }
