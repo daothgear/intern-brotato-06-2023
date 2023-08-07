2f0fe7b9c82226f75a76ba10624cdc00fd5c0c81
Quang Huy <136431938+GearIncQhuy@users.noreply.github.com>
2023-08-07 23:45:58 +0700

    Fixed add weapons with attack point position
    Fixed add weapons with attack point position

--- Assets/_Game/Scripts/Player/PlayerWeapon.cs
+++ Assets/_Game/Scripts/Player/PlayerWeapon.cs
@@ -1,34 +1,37 @@
-using com.ootii.Messages;
+﻿using com.ootii.Messages;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour {
-  [SerializeField] private Transform[] weaponPositions = new Transform[6];
+  public List<Transform> weaponPositions = new List<Transform>();
   private List<GameObject> collectedWeapons = new List<GameObject>();
   public GameObject weaponPrefab;
   private int nextAvailableWeaponIndex = 1;
 
   private void Start() {
-    MessageDispatcher.AddListener(Constants.Mess_addWeapon, AddWeapon);
+    MessageDispatcher.AddListener(Constants.Mess_addWeapon , AddWeapon);
 
-    if (weaponPositions.Length > 0 && weaponPositions[0] != null) {
-      CreateWeaponAtPosition(weaponPrefab, weaponPositions[0]);
+    if (weaponPositions.Count > 0 && weaponPositions[0] != null) {
+      CreateWeaponAtPosition(weaponPrefab , weaponPositions[0]);
     }
   }
 
-  public void AddWeapon(IMessage msg) { 
-    if (nextAvailableWeaponIndex < weaponPositions.Length && weaponPositions[nextAvailableWeaponIndex] != null) {
-      CreateWeaponAtPosition(weaponPrefab, weaponPositions[nextAvailableWeaponIndex]);
+  private void Update() {
+    CheckAndMergeWeapons();
+  }
+
+  public void AddWeapon(IMessage msg) {
+    if (nextAvailableWeaponIndex < weaponPositions.Count && weaponPositions[nextAvailableWeaponIndex] != null) {
+      CreateWeaponAtPosition(weaponPrefab , weaponPositions[nextAvailableWeaponIndex]);
       nextAvailableWeaponIndex++;
-      CheckAndMergeWeapons();
     } else {
       ShowWarningForMissingWeaponPosition();
     }
   }
 
   private void CheckAndMergeWeapons() {
-    for (int i = 0; i < collectedWeapons.Count; i++) {
-      for (int j = i + 1; j < collectedWeapons.Count; j++) {
+    for (int i = 0 ; i < collectedWeapons.Count ; i++) {
+      for (int j = i + 1 ; j < collectedWeapons.Count ; j++) {
         GameObject weaponA = collectedWeapons[i];
         GameObject weaponB = collectedWeapons[j];
         Weapon weaponComponentA = weaponA.GetComponent<Weapon>();
@@ -38,20 +41,21 @@ public class PlayerWeapon : MonoBehaviour {
           collectedWeapons.RemoveAt(j);
           Destroy(weaponB);
           weaponComponentA.currentWeaponLevel++;
-          WeaponDataLoader.Ins.LoadWeaponInfo(weaponComponentA.currentWeaponId, weaponComponentA.currentWeaponLevel);
+          WeaponDataLoader.Ins.LoadWeaponInfo(weaponComponentA.currentWeaponId , weaponComponentA.currentWeaponLevel);
+          nextAvailableWeaponIndex--;
         }
       }
     }
   }
 
-  private void CreateWeaponAtPosition(GameObject weaponPrefab, Transform position) {
-    GameObject newWeapon = Instantiate(weaponPrefab, position.position, position.rotation);
+  private void CreateWeaponAtPosition(GameObject weaponPrefab , Transform position) {
+    GameObject newWeapon = Instantiate(weaponPrefab , position.position , position.rotation);
     newWeapon.transform.parent = position;
     collectedWeapons.Add(newWeapon);
     Debug.Log("Weapon added at position: " + position.name);
   }
 
   private void ShowWarningForMissingWeaponPosition() {
-    Debug.LogWarning("Not enough weapon positions or position missing in weaponPositions array!");
+    Debug.LogWarning("Not enough weapon positions or position missing in weaponPositions list!");
   }
 }
