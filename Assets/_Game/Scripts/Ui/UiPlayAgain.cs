9ba3da9391ad3ebbb6b88302ff1df80a4f9966c9
Qhuy9701 <136431938+GearIncQhuy@users.noreply.github.com>
2023-08-11 10:35:17 +0700

    RenameFile
    RenameFile

--- Assets/_Game/Scripts/Ui/UiPlayAgain.cs
+++ Assets/_Game/Scripts/Ui/UiPlayAgain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class UiPlayAgaint : MonoBehaviour
+public class UiPlayAgain : MonoBehaviour
 {
   public void PLayAgaint() {
     SceneManager.LoadScene(Constants.Scene_StartGame);
