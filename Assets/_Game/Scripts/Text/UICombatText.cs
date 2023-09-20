69a1c234d2c18a5e4a41d73ad6a4fb11844b9df6
GearIncQhuy <136431938+GearIncQhuy@users.noreply.github.com>
2023-09-20 23:17:22 +0700

    Create Pool for UiCombatText.
    Create Pool for UiCombatText.

--- Assets/_Game/Scripts/Text/UICombatText.cs
+++ Assets/_Game/Scripts/Text/UICombatText.cs
@@ -7,10 +7,20 @@ public class UICombatText : MonoBehaviour {
   [SerializeField] private TextMeshProUGUI damageText;
   [SerializeField] private Vector3 offset;
   [SerializeField] private float timeDestroy = 0.3f;
-  public void SetUp(Vector3 position , string text , Color color) {
+  private float startTime;
+  private void Start() {
+    startTime = Time.time;
+  }
+
+  public void SetUp(Vector3 position, string text, Color color) {
     transform.position = position + offset;
     damageText.text = text;
     damageText.color = color;
-    Destroy(gameObject,timeDestroy);
+  }
+
+  private void Update() {
+    if (Time.time - startTime >= timeDestroy) {
+      ObjectPool.Ins.ReturnToPool(Constants.Tag_CombatText, gameObject);
+    }
   }
 }
--- Assets/_Game/Scripts/Text/UICombatTextManager.cs
+++ Assets/_Game/Scripts/Text/UICombatTextManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
 public class UICombatTextManager : MonoBehaviour {
-    [SerializeField] private UICombatText uicombatTextPrefabs;
-    [SerializeField] private Transform canvasText;
-    public void CreateUICombatText(Vector3 position, string text , Color color) {
-        UICombatText uiCombatText = Instantiate(uicombatTextPrefabs, canvasText);
-        uiCombatText.SetUp(position, text, color);
-    }
+  [SerializeField] private Transform canvasText;
+  public void CreateUICombatText(Vector3 position, string text, Color color) {
+    GameObject uiCombatTextObject = ObjectPool.Ins.SpawnFromPool(Constants.Tag_CombatText, position, Quaternion.identity);
+    UICombatText uiCombatText = uiCombatTextObject.GetComponent<UICombatText>();
+    uiCombatText.transform.SetParent(canvasText);
+    uiCombatText.transform.localScale = Vector3.one;
+    uiCombatText.SetUp(position, text, color);
+  }
 }
