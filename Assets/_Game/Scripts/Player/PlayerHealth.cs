8da4e2f84c8360ddbccd060130957bc2ddf257a7
GearIncQhuy <136431938+GearIncQhuy@users.noreply.github.com>
2023-10-11 02:44:15 +0700

    Upload Logic list weapon when user exits the game final.
    Upload Logic list weapon when user exits the game final.

--- Assets/_Game/Scripts/Player/PlayerHealth.cs
+++ Assets/_Game/Scripts/Player/PlayerHealth.cs
@@ -36,12 +36,28 @@ public class PlayerHealth : MonoBehaviour {
 
   private void Die() {
     player.die = true;
+
+    player.lastWeapon.Clear();
+    foreach (GameObject weaponObject in player.collectedWeapons) {
+      Weapon weaponComponent = weaponObject.GetComponent<Weapon>();
+      int weaponID = weaponComponent.currentWeaponId;
+      int weaponLevel = weaponComponent.currentWeaponLevel;
+      WeaponData.WeaponInfo weaponInfo = WeaponDataLoader.Ins.LoadWeaponInfo(weaponID, weaponLevel);
+      if (weaponInfo != null) {
+        player.lastWeapon.Add(weaponInfo);
+      }
+    }
+
     //level player
     ReferenceHolder.Ins.playerExp.SaveLevel(0);
     //save time
     ReferenceHolder.Ins.timeManager.SavePlayerPrefsData(1);
     //coin
     ReferenceHolder.Ins.playerCoin.SaveCoinAmount(500);
+    //weapon
+    ReferenceHolder.Ins.playerWeapon.ResetWeapon();
+    //save data
+    ReferenceHolder.Ins.playerWeapon.SaveCollectedWeapons();
     ReferenceHolder.Ins.uicontroller.UiEndGame.SetActive(player.die);
     ReferenceHolder.Ins.uicontroller.ShowEndGame();
   }
